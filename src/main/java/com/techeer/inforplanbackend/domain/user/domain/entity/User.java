a09f7f863fa3b7fdaa9069bca8cdfd55a311cb7a
Maeng <msuyun1010@ewhain.net>
2022-05-04 01:58:24 +0900

    Feat: User, UserRepository 수정

--- src/main/java/com/techeer/inforplanbackend/domain/user/domain/entity/User.java
+++ src/main/java/com/techeer/inforplanbackend/domain/user/domain/entity/User.java
@@ -2,18 +2,37 @@ package com.techeer.inforplanbackend.domain.user.domain.entity;
 
 import lombok.Builder;
 
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
-    @Id
+    @Id @GeneratedValue
+    private Long id;
     private String email;
+    private String password;
     private String name;
     private String number;
     private String url;
     private java.sql.Date birth;
     private java.sql.Date created_at;
     private java.sql.Date update_at;
+
+    protected User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- src/main/java/com/techeer/inforplanbackend/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/techeer/inforplanbackend/domain/user/domain/repository/UserRepository.java
@@ -3,7 +3,12 @@ package com.techeer.inforplanbackend.domain.user.domain.repository;
 import com.techeer.inforplanbackend.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, String> {
     //Optional User<>
+    List<User> findByEmail(String email);
+
+    User findById(long id);
     
 }
