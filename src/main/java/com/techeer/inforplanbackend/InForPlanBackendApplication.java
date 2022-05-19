b16b2bfb83a9d74102fa587c65c10a298fb02bfa
Maeng <msuyun1010@ewhain.net>
2022-05-19 16:31:12 +0900

    Feat: build.gradle, application.java 수정

--- src/main/java/com/techeer/inforplanbackend/InForPlanBackendApplication.java
+++ src/main/java/com/techeer/inforplanbackend/InForPlanBackendApplication.java
@@ -1,12 +1,12 @@
 package com.techeer.inforplanbackend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@EntityScan(basePackages = {"com.techeer.inforplanbackend"})
 @SpringBootApplication
 public class InForPlanBackendApplication {
 
