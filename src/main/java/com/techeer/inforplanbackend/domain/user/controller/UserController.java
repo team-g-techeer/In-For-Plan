1bccbedaca5f572b21f0dd30d49803ed32cc116b
choiseyeon <choi20014830@gmail.com>
2022-05-30 22:36:50 +0900

    Feat: 토큰 발행

--- src/main/java/com/techeer/inforplanbackend/domain/user/controller/UserController.java
+++ src/main/java/com/techeer/inforplanbackend/domain/user/controller/UserController.java
@@ -2,20 +2,17 @@ package com.techeer.inforplanbackend.domain.user.controller;
 
 import com.techeer.inforplanbackend.domain.user.domain.entity.Users;
 import com.techeer.inforplanbackend.domain.user.dto.Mapper.UserMapper;
-import com.techeer.inforplanbackend.domain.user.dto.Request.UserRequestDto;
+import com.techeer.inforplanbackend.domain.user.dto.Request.JwtRequest;
+import com.techeer.inforplanbackend.domain.user.dto.Request.UserCreateRequestDto;
+import com.techeer.inforplanbackend.domain.user.dto.Response.JwtResponse;
 import com.techeer.inforplanbackend.domain.user.dto.Response.UserResponseDto;
 import com.techeer.inforplanbackend.domain.user.service.UserService;
+import com.techeer.inforplanbackend.global.config.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,9 +23,11 @@ public class UserController {
 
     public final UserMapper userMapper;
 
+    private final JwtTokenUtil jwtTokenUtil;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserResponseDto create(@Validated @RequestBody UserRequestDto userRequestDto) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public UserResponseDto create(@Validated @RequestBody UserCreateRequestDto userRequestDto){
 
         Users user = userService.create(userRequestDto);
 
@@ -41,4 +40,19 @@ public class UserController {
         return userService.findById(id);
 
     }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+        final Users user = userService.authenticateByEmailAndPassword
+                (authenticationRequest.getEmail(), authenticationRequest.getPassword());
+        final String token = jwtTokenUtil.generateToken(user.getEmail());
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+//    @PostMapping("/login")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public UserResponseDto Login(@Validated @RequestBody UserLoginRequestDto userLoginRequestDto) {
+//
+//
+//
+//    }
 }
--- src/main/java/com/techeer/inforplanbackend/domain/user/repository/UserRepository.java
+++ src/main/java/com/techeer/inforplanbackend/domain/user/repository/UserRepository.java
@@ -1,9 +1,12 @@
-package com.techeer.inforplanbackend.domain.user.domain.repository;
+package com.techeer.inforplanbackend.domain.user.repository;
 
 import com.techeer.inforplanbackend.domain.user.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByEmail(String email);
 }
